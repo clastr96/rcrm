ef8877e65330ba72807fe70d8241f8df586e2e86
Radek Provaznik <radek.provaznik@3pillarglobal.com>
2024-04-23 17:13:39 +0200

    init commit

---
+++ src/main/java/com/example/raynetcrm/RaynetCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.raynetcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaynetCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaynetCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/raynetcrm/RaynetCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raynetcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaynetCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
